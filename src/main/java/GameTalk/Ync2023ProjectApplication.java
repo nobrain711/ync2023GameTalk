a6f870381ab6ef0f5663775875c457f3ff23685e
nobrain711 <qa9975@ync.ac.kr>
2023-10-17 10:33:08 +0900

    first upload

---
+++ src/main/java/GameTalk/Ync2023ProjectApplication.java
@@ -0,0 +1,13 @@
+package GameTalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ync2023ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ync2023ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GameTalk/Ync2023ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package GameTalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ync2023ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
